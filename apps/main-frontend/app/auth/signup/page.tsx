d6bd72a66f20ad0846aeff934b2f074d0250fd40
manik prakash <manikprakash74@gmail.com>
2025-09-27 22:08:14 +0530

    auth frontend

--- apps/main-frontend/app/auth/signup/page.tsx
+++ apps/main-frontend/app/auth/signup/page.tsx
@@ -1,9 +1,11 @@
-export default function signup() {
-    return(
-        <>
-            <h1>signup page</h1>
-        
-        </>
-    );
-    
+import { SignupForm } from "@/components/signup-form"
+
+export default function SignupPage() {
+  return (
+    <div className="bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+      <div className="w-full max-w-sm">
+        <SignupForm />
+      </div>
+    </div>
+  )
 }
